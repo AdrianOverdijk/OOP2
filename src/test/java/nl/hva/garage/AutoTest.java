0061e57041d8d6e1128cf0e34047c1f258b8498f
Adrian Overdijk <adrian.overdijk@hva.nl>
2021-06-21 20:40:13 +0200

    Create Tests

---
+++ src/test/java/nl/hva/garage/AutoTest.java
@@ -0,0 +1,24 @@
+package nl.hva.garage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutoTest {
+
+    @Test
+    void setAantalCilinders() {
+    }
+
+    @Test
+    void maxSnelheid() {
+    }
+
+    @Test
+    void kosten() {
+    }
+
+    @Test
+    void reparatiekosten() {
+    }
+}
