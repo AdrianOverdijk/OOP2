472a126a0a53589d5b174170e6d1aa96dc20ae7e
Adrian Overdijk <adrian.overdijk@hva.nl>
2021-06-21 21:42:51 +0200

    Create test

--- src/test/java/nl/hva/garage/AutoTest.java
+++ src/test/java/nl/hva/garage/AutoTest.java
@@ -1,5 +1,6 @@
 package nl.hva.garage;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,18 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class AutoTest {
 
     @Test
-    void setAantalCilinders() {
+    void bij_geen_cylinders_geef_een_error() {
+        Auto kadet = new Auto("kadet", 4, 4);
+        kadet.setAantalCilinders(0);
+
+        int actual = 4;
+        Assertions.assertEquals(kadet.getAantalCilinders(), actual);
+
     }
 
     @Test
-    void maxSnelheid() {
+    void klopt_de_berekening_van_maximale_snelheid() throws Exception {
+        Auto kadet = new Auto("kadet", 4, 4);
+
+        int actual = 160;
+        Assertions.assertEquals(kadet.maxSnelheid(), actual);
     }
 
-    @Test
-    void kosten() {
-    }
 
     @Test
-    void reparatiekosten() {
+    void repartiekosten_moet_goed_zijn() {
+        Auto kadet = new Auto("kadet", 4, 4);
+        kadet.kosten(50);
+
+        int actual = 120;
+        Assertions.assertEquals(kadet.reparatiekosten(), actual);
     }
 }
