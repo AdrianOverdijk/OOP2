fbe4d98b356fc841875f3d55ae58d691d841a703
Adrian Overdijk <adrian.overdijk@hva.nl>
2021-06-30 10:01:49 +0200

    Rename to be singular

--- src/main/java/nl/hva/garage/ShowVoertuig.java
+++ src/main/java/nl/hva/garage/ShowVoertuig.java
@@ -1,7 +1,7 @@
 package nl.hva.garage;
 import io.javalin.Javalin;
 
-public class ShowVoertuigen {
+public class ShowVoertuig {
     public static void main(String[] args) {
         //Create App
         Javalin app = Javalin.create().start(8000);
